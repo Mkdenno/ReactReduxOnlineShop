a3f1e4852e929e153bbba5abfa02e45e20565227
Mkdenno <denniskiprop25@gmail.com>
2023-02-25 11:01:53 +0300

    Initial

---
+++ backend/products.js
@@ -0,0 +1,48 @@
+const products=[
+    {
+        id:1,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228082/online/iphone12_t3bwbh.jpg"
+    },
+    {
+        id:2,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228082/online/iphone12pro_a127ry.jpg"
+    },
+    {
+        id:3,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228082/online/iphone12_t3bwbh.jpg"
+    },
+    {
+        id:4,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228090/online/galaxyS_w66xqt.png"
+    },
+    {
+        id:5,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228082/online/iphone12pro_a127ry.jpg"
+    },
+    {
+        id:6,
+        name:"iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/doemqfk0k/image/upload/v1677228090/online/galaxyS_w66xqt.png"
+    }
+
+];
+
+
+module.exports = products
